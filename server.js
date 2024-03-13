1ee37a3fc0152ab67b1bd195fa492ed085aea6b1
mukeshyadav7 <97288411+mukeshyadav7@users.noreply.github.com>
2024-03-13 23:12:51 +0530

    Update server.js
    name changed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Transflower Store</h1>');
+  res.end('<h1>Mukesh Yadav is a survivor</h1>');
 });
 server.listen(7000, () => {
   console.log(`Server running at 7000`);
